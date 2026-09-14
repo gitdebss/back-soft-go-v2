b104f29fed7fc64207632f9873313258af39d2aa
Débora Martins de Oliveira <deboramartinsdeoliveira2008@gmail.com>
2026-09-14 18:26:13 -0300

    acho que precisava desse arquivo kk

---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+    constructor(private readonly configService: ConfigService) { }
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: this.configService.get<string>('DB_HOST'),
+            port: Number(this.configService.get<string>('DB_PORT')),
+            username: this.configService.get<string>('DB_USERNAME'),
+            password: this.configService.get<string>('DB_PASSWORD'),
+            database: this.configService.get<string>('DB_NAME'),
+            autoLoadEntities: true,
+            synchronize: false
+        };
+    }
+}
