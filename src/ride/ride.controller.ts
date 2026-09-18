e36cabd0fb66659a797f0bf34bdf82a8a21f968c
Débora Martins de Oliveira <deboramartinsdeoliveira2008@gmail.com>
2026-09-17 21:06:11 -0300

    docs: adicionando exemplos de bodys nos endpoints post e descrições em todos

--- src/ride/ride.controller.ts
+++ src/ride/ride.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { CreateRideDto } from './dto/create-ride.dto.js';
 import { RideService } from './ride.service.js';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('rides')
 export class RideController {
@@ -9,19 +10,37 @@ export class RideController {
     ) { }
 
     @Get()
-    async getRides(@Query('transportType') transportType?: string, @Query('date') date?: string) { 
+    @ApiOperation({ summary: 'Retorna todas as corridas. É possível filtrar por tipo de transporte e data da corrida' })
+    async getRides(@Query('transportType') transportType?: string, @Query('date') date?: string) {
         const rides = await this.rideService.getRides(transportType, date)
         return rides;
     }
 
     @Get('/:id')
-    async getRideById(@Param('id', ParseIntPipe) id: number) { 
+    @ApiOperation({ summary: 'Retorna a corrida com o id passado na url' })
+    async getRideById(@Param('id', ParseIntPipe) id: number) {
         const ride = await this.rideService.getRideById(id)
         return ride;
     }
 
     @Post()
-    async createRide(@Body() rideRequest: CreateRideDto) { 
+    @ApiOperation({ summary: 'Cria uma corrida' })
+    @ApiBody({
+        schema: {
+            example: {
+                date: '2026-09-20',
+                hour: '18:30',
+                city: 'São Leopoldo',
+                transportTypeId: 1,
+                totalSpots: 4,
+                name: 'Débora',
+                complement: 'Centro',
+                obs: 'Vou passar no mercado no caminho',
+                phone: '51999999999',
+            },
+        },
+    })
+    async createRide(@Body() rideRequest: CreateRideDto) {
         const createdRide = await this.rideService.createRide(rideRequest)
         return createdRide;
     }
--- src/user-ride/user-ride.controller.ts
+++ src/user-ride/user-ride.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { CreateUserRideDto } from './dto/create-user-ride.dto.js';
 import { UserRideService } from './user-ride.service.js';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('user-ride')
 export class UserRideController {
@@ -9,18 +10,29 @@ export class UserRideController {
     ) { }
 
     @Post('/:idRide')
+    @ApiOperation({ summary: 'Adiciona um passageiro na corrida com o id passado na url' })
+    @ApiBody({
+        schema: {
+            example: {
+                name: 'Giovanna',
+                phone: '51999999999',
+            },
+        },
+    })
     async createUserRide(@Body() userRideRequest: CreateUserRideDto, @Param('idRide', ParseIntPipe) idRide: number) {
         const createdUser = await this.userRideService.createUserRide(userRideRequest, idRide)
         return createdUser;
     }
 
     @Get()
+    @ApiOperation({ summary: 'Retorna todos os passageiros vinculados a corridas' })
     async getUserRides() {
         const users = await this.userRideService.getUserRides();
         return users;
     }
 
     @Get('/:idRide')
+    @ApiOperation({ summary: 'Retorna todos os passageiros de uma corrida com o id passado na url' })
     async getUserRidesByRideId(@Param('idRide', ParseIntPipe) idRide: number) {
         const users = await this.userRideService.getUserRidesByRideId(idRide);
         return users;
